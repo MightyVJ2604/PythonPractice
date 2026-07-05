77e5716e9e8f02eacce4c4ca50788658eedfc3cc
vijayp <vijayp@SSTLAP430.srinsofttech.lan>
2026-07-05 13:29:50 +0530

    Variables,Operators and User Input

---
+++ PythonPractice/Practice/Tutorial.py
@@ -0,0 +1,49 @@
+# Print the Word Hello World
+print("Hello World")
+
+# Data Types
+print("Data Types")
+a = 10
+b ='D'
+c="Vijay"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# Operators
+print("Operators")
+a =10
+b=20
+c=a+b
+d=a-b
+e=a*b
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# Casting
+print("Casting")
+a="10"
+b="20"
+c=a+b
+print(c)
+print("After Casting")
+
+a=int("10")
+b=int("20")
+c=a+b
+print(c)
+
+# User Input
+print("User Input")
+a=10
+b= int (input())
+c=a+b
+print(c)
+
+
+
