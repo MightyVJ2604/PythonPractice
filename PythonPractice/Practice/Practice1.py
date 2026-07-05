15f365b3ee2348391dbf6774ecc1f9c830d5660a
vijayp <vijayp@SSTLAP430.srinsofttech.lan>
2026-07-05 13:31:13 +0530

    commit

---
+++ PythonPractice/Practice/Practice1.py
@@ -0,0 +1,33 @@
+'''# Code1 (Get the Input of Name and Age from the User and Print It)
+Name = input()
+Age = input()
+
+print("My Name is ", Name)
+print("My Age is ", Age)
+
+# Code2 (Get the Input of Name,Age and Address from the User and Print It)
+
+Name = input()
+Age = input()
+Address = input()
+
+
+print("My Name is ", Name)
+print("My Age is ", Age)
+print("My Address is ",Address)  '''
+
+# Code 3
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+Multiply = a*b*c
+Add = a+b+c
+
+Divide = Multiply/Add
+
+print(Multiply)
+print(Add)
+print(Divide)
+
